e4855445db9d971787dd76efb081801e2840cd59
Won Jang <122013694+woooon0@users.noreply.github.com>
2025-05-18 19:48:50 +0900

    Title: 퀵 정렬 구현, Time: 385ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250518/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+arr = quick_sort(arr)
+print(*arr, sep=' ')
