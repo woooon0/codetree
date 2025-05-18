22d5459504235e19e5b28e1c20824ab73f011cf0
Won Jang <122013694+woooon0@users.noreply.github.com>
2025-05-18 13:34:00 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250518/황금비율 토스트/golden-toast.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+char s[100005];
+
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    scanf("%s", s);
+    int maxn = n;
+    for (int i = 0; i < m; i++) {
+        char command;
+        scanf(" %c", &command);
+        if (command == 'P') {
+            char bread;
+            scanf(" %c", &bread);
+            if(n ==maxn){
+                s[n] = bread;
+            }
+            else{
+                for(int mn = maxn;n<mn;mn--){
+                    s[mn] = s[mn-1];
+                }
+            }
+        }
+        if(command == 'L'){
+            if(n != 0){
+                n--;
+            }
+        }
+        if(command == 'R'){
+            if(n != maxn){
+                n ++;
+        }
+        if(command == 'D'){
+            if(n != maxn){
+                for(n;n=<maxn;n++){
+                    s[n] = s[n+1];
+                }
+            }
+        }
+    }
+
+
+    
+    // Please write your code here.
+    return 0;
+}
